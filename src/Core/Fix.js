147b2c435f1df800a8780ac713a4e65fd7481135
Ostkaka <ostkaka@localhost.localdomain>
2016-09-06 19:23:15 +0200

    Code cleaned. src/GLChunk.js created(untested). src/Core/Map2D.js created.

--- src/Core/Fix.js
+++ src/Core/Fix.js
@@ -1,29 +1,30 @@
+var fix = {};
 // Convert float to 32 bit fixed point with 16 bit fraction.
 // Must be done after each operation.
-// Example: fix(fix(fix(a*b)/3)+14.3)
-function fix(a) {
-    return (Math.floor(a*0x10000)%0x100000000)/0x10000);
+// Example: toFix(toFix(toFix(a*b)/3)+14.3)
+function toFix(a) {
+    return (Math.floor(a*0x10000)%0x100000000)/0x10000;
 }
 
-function isFix(a) {
-    return (fix(a) == a);
+function istoFix(a) {
+    return (toFix(a) == a);
 }
 
-function fixAdd(a, b) { return fix(a+b); }
-function fixSub(a, b) {  return fix(a-b); }
-function fixMul(a, b) {  return fix(a*b); }
-function fixDiv(a, b) { return fix(a/b); }
+fix.add = function(a, b) { return toFix(a+b); }
+fix.sub = function(a, b) {  return toFix(a-b); }
+fix.mul = function(a, b) {  return toFix(a*b); }
+fix.div = function(a, b) { return toFix(a/b); }
 
 
-function fixExp(a) { return fix(Math.exp(a)); }
-function fixLog(a) { return fix(Math.log(a)); }
-function fixPow(a, b) { return fix(Math.pow(a, b)); }
-function fixSqrt(a) { return fix(Math.sqrt(a)); }
+fix.exp = function(a) { return toFix(Math.exp(a)); }
+fix.log = function(a) { return toFix(Math.log(a)); }
+fix.pow = function(a, b) { return toFix(Math.pow(a, b)); }
+fix.sqrt = function(a) { return toFix(Math.sqrt(a)); }
 
-function fixSin(a) { return fix(Math.sin(a)); }
-function fixCos(a) { return fix(Math.cos(a)); }
-function fixTan(a) { return fix(Math.tan(a)); }
-function fixAsin(a) { return fix(Math.asin(a)); }
-function fixAcos(a) { return fix(Math.acos(a)); }
-function fixAtan(a) { return fix(Math.atan(a)); }
-function fixAtan2(a, b) { return fix(Math.atan2(a, b)); }
+fix.sin = function(a) { return toFix(Math.sin(a)); }
+fix.cos = function(a) { return toFix(Math.cos(a)); }
+fix.tan = function(a) { return toFix(Math.tan(a)); }
+fix.asin = function(a) { return toFix(Math.asin(a)); }
+fix.acos = function(a) { return toFix(Math.acos(a)); }
+fix.atan = function(a) { return toFix(Math.atan(a)); }
+fix.atan2 = function(a, b) { return toFix(Math.atan2(a, b)); }
--- src/Core/v2.js
+++ src/Core/v2.js
@@ -1,19 +1,20 @@
 v2 = {};
 
+console.log("v2 here");
 v2.create = function(x, y) {
-	var v = [x, y];
+	var v = [toFix(x), toFix(y)];
 	
-	// Temporary properties
-	Object.defineProperties(v, {
-		x: {
-			get: function () { return v[0]; },
-			set: function (value) { v[0] = value; }
-		},
-		y: {
-			get: function () { return v[1]; },
-			set: function (value) { v[1] = value; }
-		},
-	});
+//	// Temporary properties
+//	Object.defineProperties(v, {
+//		x: {
+//			get: function () { return v[0]; },
+//			set: function (value) { v[0] = value; }
+//		},
+//		y: {
+//			get: function () { return v[1]; },
+//			set: function (value) { v[1] = value; }
+//		},
+//	});
 	
 	return v;
 }
@@ -23,44 +24,44 @@ v2.clone = function(a) {
 }
 
 v2.add = function(a, b, out) {
-	out[0] = a[0]+b[0];
-	out[1] = a[1]+b[1];
+	out[0] = fix.add(a[0],b[0]);
+	out[1] = fix.add(a[1],b[1]);
 }
 
-v2.subtract = function(a, b, out) {
-	out[0] = a[0]-b[0];
-	out[1] = a[1]-b[1];
-}
-
-/* c: constant - float
- * a: v2     - float[2]
- */
-v2.multiply = function(c, a, out) {
-	out[0] = c*a[0];
-	out[1] = c*a[1];
+v2.sub = function(a, b, out) {
+	out[0] = fix.sub(a[0], b[0]);
+	out[1] = fix.sub(a[1], b[1]);
 }
 
 /* c: constant - float
  * a: v2     - float[2]
  */
-v2.divide = function(a, c, out) {
-	out[0] = a[0]/c;
-	out[1] = a[1]/c;
+v2.mul = function(c, a, out) {
+	out[0] = fix.mul(c, a[0]);
+	out[1] = fix.mul(c, a[1]);
+}
+
+/* c: constant - float
+ * a: v2     - float[2]
+ */
+v2.div = function(a, c, out) {
+	out[0] = fix.div(a[0], c);
+	out[1] = fix.div(a[1], c);
 }
 
 /* c: constant - float
  * a: v2     - float[2]
  */
 v2.mod = function (a, c, out) {
-	out[0] = a[0]%c;
-	out[1] = a[1]%c;
+	out[0] = a[0]%toFix(c);
+	out[1] = a[1]%toFix(c);
 }
 
 
 v2.normalize = function(a, out) {
 	var length = v2.length(a);
-	out[0] = a[0]/length;
-	out[1] = a[1]/length;
+	out[0] = fix.div(a[0], length);
+	out[1] = fix.div(a[1], length);
 }
 
 
@@ -68,23 +69,24 @@ v2.normalize = function(a, out) {
  * Float output functions:
  *****************************************/
 v2.length = function(a) {
-	return Math.sqrt(a[0]*a[0] + a[1]*a[1]);
+	return fix.sqrt(fix.add(fix.mul(a[0], a[0]), fix.mul(a[1], a[1])));
 }
 
 v2.lengthSquared = function(a) {
-	return a[0]*a[0] + a[1]*a[1];
+	return fix.add(fix.mul(a[0], a[0]), fix.mul(a[1], a[1]));
 }
 
 v2.dot = function(a, b) {
-	return a[0]*b[0] + a[1]*b[1];
+	return fix.add(fix.mul(a[0], b[0]), fix.mul(a[1], b[1]));
 }
 
 v2.clampF = function(a, min, max) {
+    min = toFix(min);
+    max = toFix(max);
 	if (min <= max) {
 		return Math.max(Math.min(a, max), min);
 	}
 	else {
 		return Math.max(Math.min(a, min), max);
-	}
-	
+	}	
 }
