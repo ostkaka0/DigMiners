ac7744ebf671b883f4ab8ab382d9f5c35496e9f5
John <john.3manuelsson@gmail.com>
2016-09-14 17:17:18 +0200

    tests/TestMovement.js created.

--- unit_tests/UTestEntityWorld.js
+++ unit_tests/UTestEntityWorld.js
@@ -1,23 +1,15 @@
 g_unitTests.testEntityWorld = function() {
 	var entityWorld = new EntityWorld();
     var entity = entityWorld.add();
-    console.log("Entity id: " + entity.id);
-    console.log("Entity isActive: " + entity.isActive);
     if (entity.isActive || entityWorld.entityArray.length > 0 || entityWorld.freeIdList.length > 0 || entityWorld.nextId != 1)
         return false;
     entityWorld.update();
-    console.log("Entity id: " + entity.id);
-    console.log("Entity isActive: " + entity.isActive);
     if (!entity.isActive || entityWorld.entityArray.length != 1 || entityWorld.freeIdList.length > 0 || entityWorld.nextId != 1) 
         return false;    
     entityWorld.remove(entity);
-    console.log("Entity id: " + entity.id);
-    console.log("Entity isActive: " + entity.isActive);
     if (!entity.isActive || entityWorld.entityArray.length != 1 || entityWorld.freeIdList.length > 0 || entityWorld.nextId != 1) 
         return false;
     entityWorld.update();
-    console.log("Entity id: " + entity.id);
-    console.log("Entity isActive: " + entity.isActive);
     if (entity.isActive || entityWorld.entityArray.length > 0 || entityWorld.freeIdList.length != 1 || entityWorld.nextId != 1) 
         return false;
 	return true;
