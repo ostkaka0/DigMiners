47b804ac5f019e85427ac3042d2869dd21c22ced
Gustav Hammar <gustav9797@hotmail.se>
2016-09-24 19:48:52 +0200

    Dig "implemented", player.setName

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,14 @@
+
 Player = function (playerId, entityId, name) {
     this.playerId = playerId;
     this.entityId = entityId;
     this.name = name;
+}
+
+Player.prototype.setName = function (name, gameData) {
+    var entity = gameData.entityWorld.objects[this.entityId];
+    if (player.text)
+        entity.drawable.removeSprite("username");
+    this.text = new PIXI.Text(name, { fill: '#ffffff' });
+    entity.drawable.addSprite("username", this.text, v2.create(- this.text.width / 2, -80), false);
 }
