19b2adf1e377190c0ad2e31a61189b2622e17f9e
John <john.3manuelsson@gmail.com>
2016-09-24 10:33:22 +0200

    GameData.js created, game directory created

--- DigMiners.js
+++ DigMiners.js
@@ -7,14 +7,9 @@ renderer.view.style.left = '0%';
 renderer.view.style.top = '0%';
 document.body.appendChild(renderer.view);
 
-var playerWorld = new ObjectWorld();
-var entityWorld = new ObjectWorld();
-var tileWorld = new Map2D();
-var generator = new Generator(1337);
-var chunkRenderer = new ChunkRenderer(gl, tileWorld, 32.0);
+var gameData = new GameData();
 var camera = new Camera();
-var gameData = { playerWorld: playerWorld, entityWorld: entityWorld, tileWorld: tileWorld };
-var commandTypes = new CommandsTypes([CommandPlayerMove, CommandEntityStatus]);
+var chunkRenderer = new ChunkRenderer(gl, gameData.tileWorld, 32.0);
 
 onTexturesLoadProgress = function (name, file, progress) {
 
@@ -23,14 +18,14 @@ onTexturesLoadProgress = function (name, file, progress) {
 onTexturesLoadComplete = function () {
     // Must wait until all textures have loaded to continue! important
 
-    client = new Client("127.0.0.1", 3000);
+    client = new Client(gameData, "127.0.0.1", 3000);
 }
 var textureManager = new TextureManager();
 
 loadGame = function () {
-    for (var x = -8; x < 8; ++x) {
-        for (var y = -8; y < 8; ++y) {
-            loadChunk(tileWorld, x, y);
+    for (var x = -2; x < 2; ++x) {
+        for (var y = -2; y < 2; ++y) {
+            loadChunk(gameData.tileWorld, x, y);
         }
     }
 
@@ -65,18 +60,18 @@ loadGame = function () {
     });
 
     // Start gameLoop
-    gameLoop(tick, render);
+    gameLoop(tick, render, gameData.tickDuration);
 }
 
 loadChunk = function (world, x, y) {
     var chunk = new Chunk();
-    generator.generate(chunk, x, y);
+    gameData.generator.generate(chunk, x, y);
     world.set(x, y, chunk);
 }
 
 tick = function (dt) {
     //console.log(dt);
-    entityWorld.objectArray.forEach(function (entity) {
+    gameData.entityWorld.objectArray.forEach(function (entity) {
         if (entity.physicsBody && entity.physicsBody.angle)
             entity.physicsBody.angleOld = entity.physicsBody.angle;
     });
@@ -85,10 +80,10 @@ tick = function (dt) {
         command.execute(gameData);
     });
     commands.length = 0;
-    playerWorld.update();
+    gameData.playerWorld.update();
     entityFunctionPlayerMovement(gameData, dt);
     entityFunctionPhysicsBodySimulate(gameData, dt);
-    entityWorld.update();
+    gameData.entityWorld.update();
 }
 
 render = function (tickFracTime) {
@@ -105,9 +100,9 @@ render = function (tickFracTime) {
     var viewMatrix = PIXI.Matrix.IDENTITY.clone();
     viewMatrix = viewMatrix.translate(-camera.frustrum.x, -camera.frustrum.y);
     viewMatrix = viewMatrix.scale(2 / canvas.width, 2 / canvas.height);
-    chunkRenderer.render(tileWorld, projectionMatrix.clone().append(viewMatrix), camera);
+    chunkRenderer.render(gameData.tileWorld, projectionMatrix.clone().append(viewMatrix), camera);
 
-    entityWorld.objectArray.forEach(function (entity) {
+    gameData.entityWorld.objectArray.forEach(function (entity) {
         if (entity.physicsBody && entity.drawable) {
             var x = -camera.frustrum.x + canvas.width / 2 + tickFracTime * 32.0 * entity.physicsBody.pos[0] + (1 - tickFracTime) * 32.0 * entity.physicsBody.posOld[0];
             var y = camera.frustrum.y + canvas.height / 2 - (tickFracTime * 32.0 * entity.physicsBody.pos[1] + (1 - tickFracTime) * 32.0 * entity.physicsBody.posOld[1]);
