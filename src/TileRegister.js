1e844038119348fafa95bddec9a47894618bc20c
ostkaka <john.ostkaka@gmail.com>
2016-09-24 17:25:32 +0200

    TileWorld functions created.

--- src/TileRegister.js
+++ src/TileRegister.js
@@ -3,6 +3,14 @@ TileRegister = function() {
     this.mapNameToType = {};
 }
 
+TileType = function(id, name, isSolid, isOre, hardness) {
+    this.id = id;
+    this.name = name;
+    this.isSolid = isSolid;
+    this.isOre = isOre;
+    this.hardness = hardness;
+}
+
 TileRegister.prototype.register = function(name, isSolid, isOre, hardness) {
     var id = this.tileTypes.length;
     var tileType = new TileType(id, name, isSolid, isOre, hardness);
