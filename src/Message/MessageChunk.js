77a07077773da001e0a551d2c9c78db156175794
Gustav Hammar <gustav9797@hotmail.se>
2016-09-25 10:22:09 +0200

    Almost works

--- src/Message/MessageChunk.js
+++ src/Message/MessageChunk.js
@@ -1,5 +1,6 @@
 
 MessageChunk = function (chunk, chunkX, chunkY) {
+    var chunk = chunk;
     if (!chunk)
         chunk = new Chunk();
     /*var chunkClone = JSON.parse(JSON.stringify(chunk));
@@ -7,8 +8,7 @@ MessageChunk = function (chunk, chunkX, chunkY) {
     chunkClone.densityData.length = chunk.densityData.length;
     this.chunk = chunkClone;*/
     //TODO:CLONE!
-    if (chunk)
-        this.chunk = chunk;
+    this.chunk = chunk;
     this.chunkX = chunkX;
     this.chunkY = chunkY;
 }
@@ -19,36 +19,43 @@ MessageChunk.prototype.execute = function (gameData) {
         console.error("Missing required gameData properties");
         return;
     }
+    gameData.generator.generate(this.chunk, this.chunkX, this.chunkY);
     tileWorld.set(this.chunkX, this.chunkY, this.chunk);
 }
 
 MessageChunk.prototype.serialize = function (byteArray, index) {
     serializeInt32(byteArray, index, this.chunk.densityData.length);
+    console.dir(this.chunk.densityData.length);
     serializeUint8Array(byteArray, index, this.chunk.densityData);
-    serializeInt32(byteArray, index, this.chunk.tileData.length);
-    serializeUint8Array(byteArray, index, this.chunk.tileData);
     serializeInt32(byteArray, index, this.chunkX);
-    serializeInt32(byteArray, index, this.chunkX);
+    serializeInt32(byteArray, index, this.chunkY);
+    //console.dir(byteArray);
+    console.log(this.chunk.densityData.length);
+    console.log(this.chunk.tileData.length);
+    console.log(byteArray.length);
 }
 
 MessageChunk.prototype.deserialize = function (byteArray, index) {
-    console.dir(byteArray);
     var densityLength = deserializeInt32(byteArray, index);
     this.chunk.densityData = deserializeUint8Array(byteArray, index, densityLength);
-    var tileLength = deserializeInt32(byteArray, index);
-    this.chunk.tileData = deserializeUint8Array(byteArray, index, tileLength);
     this.chunkX = deserializeInt32(byteArray, index);
     this.chunkY = deserializeInt32(byteArray, index);
+    //console.dir(byteArray);
+    console.log(this.chunk.densityData.length);
+    console.log(this.chunk.tileData.length);
+    console.log(byteArray.length);
 }
 
 MessageChunk.prototype.getSerializationSize = function () {
-    return this.chunk.densityData.length + this.chunk.tileData.length + 16;
+    return this.chunk.densityData.length + 12;
 }
 
 MessageChunk.prototype.send = function (socket) {
-    var byteArray = new Uint8Array(this.getSerializationSize());
+    //console.log(this.getSerializationSize());
+    var byteArray = new Buffer(this.getSerializationSize());
     var counter = new IndexCounter();
     this.serialize(byteArray, counter);
+    //console.log(JSON.stringify(byteArray));
     socket.emit("chunk", byteArray);
 }
 
