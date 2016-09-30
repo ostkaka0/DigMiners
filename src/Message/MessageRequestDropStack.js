f50c52359971700d2ad5bddb2759f794694264ff
Gustav Hammar <gustav9797@hotmail.se>
2016-09-30 17:42:54 +0200

    Correct displacement.

--- src/Message/MessageRequestDropStack.js
+++ src/Message/MessageRequestDropStack.js
@@ -8,11 +8,11 @@ MessageRequestDropStack.prototype.execute = function(gameData, player) {
     if(item) {
         var entity = gameData.entityWorld.objects[player.entityId];
         var physicsBody = entity.physicsBody;
-        var displacement1 = Math.random() / 5 - 0.1 + 1;
-        var displacement2 = Math.random() / 5 - 0.1 + 1;
+        var displacement1 = Math.random() / 5 - 0.1;
+        var displacement2 = Math.random() / 5 - 0.1;
         var displacement3 = Math.random() / 5 - 0.1 + 1;
         //console.log(displacement);
-        var speed = v2.create(Math.cos(displacement1 * physicsBody.angle), -Math.sin(displacement2 * physicsBody.angle));
+        var speed = v2.create(Math.cos(displacement1 + physicsBody.angle), -Math.sin(displacement2 + physicsBody.angle));
         var speed2 = {};
         v2.mul(10.0 * displacement3, speed, speed2);
         var message = new MessageItemDrop(idList.next(), item.id, item.amount, physicsBody.pos[0], physicsBody.pos[1], speed2[0], speed2[1], physicsBody.angle);
