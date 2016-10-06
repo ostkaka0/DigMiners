680d6c24083a3c8e35b69c8c6b2d43a01cda861c
ostkaka <john.ostkaka@gmail.com>
2016-10-06 15:35:09 +0200

    BlockChunk.js, Blocks.js created

---
+++ src/Blocks.js
@@ -0,0 +1,19 @@
+Blocks = {};
+
+Blocks.Null = {
+    name: "Air",
+    isSolid: false,
+    hardness: 0
+}
+
+Blocks.StonWall = {
+    name: "Stone Wall",
+    isSolid: true,
+    hardness: 1
+};
+
+Blocks.StonFloor = {
+    name: "Stone Floor",
+    isSolid: false,
+    hardness: 1
+};
