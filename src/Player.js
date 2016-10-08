4bffeb661c200cf1e9061f62b6690fc6c8559a3c
Gustav Hammar <gustav9797@hotmail.se>
2016-10-08 16:11:58 +0200

    Better player names.

--- src/Player.js
+++ src/Player.js
@@ -12,9 +12,9 @@ Player.prototype.setName = function(name, gameData) {
     var entity = gameData.entityWorld.objects[this.entityId];
     if(this.text)
         entity.drawable.removeSprite("username");
-    this.text = new PIXI.Text(name, { fill: '#ffffff' });
+    this.text = new PIXI.Text(name, { fontFamily: 'Monospace', fontSize: 15, fill: 0xffffff, align: 'center' });
     var sprite = new Sprite(null, this.text, true);
-    entity.drawable.addSprite("username", sprite, v2.create(- this.text.width / 2, -80), false);
+    entity.drawable.addSprite("username", sprite, v2.create(- this.text.width / 2, -60), false);
 }
 
 Player.prototype.getDigStrength = function() {
