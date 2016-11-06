a09cd7dbad3dd3377171d05d37c7ca2e1275cfbb
Ostkaka <ostkaka@localhost.localdomain>
2016-11-06 16:22:06 +0100

    PhysicsWorld bugs fixed

--- src/PhysicsWorld.js
+++ src/PhysicsWorld.js
@@ -59,7 +59,7 @@ PhysicsWorld.prototype.add = function(pos, velocity) {
         this.velocity.push(fixToInt32(velocity[0]), fixToInt32(velocity[1]));
         id = this.numBodies++;
     } else {
-        id = freeIds.pop();
+        id = this.freeIds.pop();
         this.pos[2*id] = fixToInt32(pos[0]);
         this.pos[2*id+1] = fixToInt32(pos[1]);
         this.velocity[2*id] = fixToInt32(velocity[0]);
@@ -72,24 +72,53 @@ PhysicsWorld.prototype.add = function(pos, velocity) {
         this.pages.set(pagePos[0], pagePos[1], [id]);
     } else {
         index = binarySearch(page, id, function(a, b) { return a-b; });
-        page.splice(index+1, 0, id);
+        page.splice(index, 0, id);
     }
     
     return id;
 }
 
 PhysicsWorld.prototype.remove = function(id) {
-    var index = binarySearch(this.freeIds, id);
+    var index = binarySearch(this.freeIds, id, function(a, b) { return a-b; });
     this.freeIds.splice(index, 0, id);
     
     var pagePos = [fixFromInt32(this.pos[2*id]) / pageDim >> 0, fixFromInt32(this.pos[2*id+1]) / pageDim >> 0];
     var page = this.pages.get(pagePos[0], pagePos[1]);
     if (page) {
-        var index = binarySearch(page, function(a, b) { return a-b; });
+        var index = binarySearch(page, id, function(a, b) { return a-b; });
         if (page[index] == id)
             page.splice(index, 1);
         if (page.length == 0)
             this.pages.set(pagePos[0], pagePos[1], undefined);
+    }
+}
+
+PhysicsWorld.prototype.getBodyAtPoint = function(point) {
+    var pagePos = [point[0] / pageDim - 0.5 >> 0, point[1] / pageDim - 0.5 >> 0];
+    for (var i = 0; i < 4; i++) {
+        var page = this.pages.get(pagePos[0] + i%2, pagePos[1] + i/2 >> 0);
+        if (!page) continue;;
+        for (var j = 0; j < page.length; j++) {
+            var id = page[j];
+            var pos = [fixFromInt32(this.pos[2*id]), fixFromInt32(this.pos[2*id+1])];
+            var length = v2.distanceSquared(pos, point);
+            if (length < 0.5)
+                return id;
+        }
+    }
+    return null;
+}
+
+PhysicsWorld.prototype.forEach = function(thisRef, userFunction) {
+    var freeId = this.freeIds[0];
+    var freeIdIndex = 0;
+    for (var id = 0; id < this.numBodies; id++) {
+        if (id == freeId) {
+            freeIdIndex++;
+            freeId = this.freeIds[freeIdIndex];
+            continue;
+        }
+        (userFunction.bind(thisRef))(id);
     }
 }
 
