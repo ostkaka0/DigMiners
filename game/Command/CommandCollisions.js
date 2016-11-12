6aec04370f4bd61b72446fc892750794d3dafd04
Ostkaka <ostkaka@localhost.localdomain>
2016-11-12 13:40:03 +0100

    CommandCollisions will sync each body only once

--- game/Command/CommandCollisions.js
+++ game/Command/CommandCollisions.js
@@ -3,6 +3,7 @@ CommandCollisions = function(collisions) {
     var physicsWorld = gameData.physicsWorld;
     var physicsEntities = gameData.physicsEntities;
     
+    var bodies = {};
     this.collisions = [];
     
     if (collisions) {
@@ -14,7 +15,14 @@ CommandCollisions = function(collisions) {
             var bPos = physicsWorld.getPos(collision[1]);
             if (aPos == undefined || bPos == undefined) return;
             
-            this.collisions.push([aEntity.id, bEntity.id, aPos, bPos]);
+            if (!bodies[collision[0]]) {
+                bodies[collision[0]] = true;
+                this.collisions.push([aEntity.id, aPos]);
+            }
+            if (!bodies[collision[1]]) {
+                bodies[collision[1]] = true;
+                this.collisions.push([bEntity.id, bPos]);
+            }
         }).bind(this));
     }
 }
@@ -24,14 +32,11 @@ CommandCollisions.prototype.execute = function(gameData) {
     var entities = gameData.entityWorld.objects;
     
     this.collisions.forEach(function(collisionData) {
-        var aEntity = entities[collisionData[0]];
-        var bEntity = entities[collisionData[1]];
-        if (!aEntity || !bEntity) return;
-        var aPhysics = aEntity.physicsBody;
-        var bPhysics = bEntity.physicsBody;
-        if (!aPhysics || !bPhysics) return;
-        physicsWorld.setPos(aPhysics.bodyId, collisionData[2]);
-        physicsWorld.setPos(bPhysics.bodyId, collisionData[3]);
+        var entity = entities[collisionData[0]];
+        if (!entity) return;
+        var physics = entity.physicsBody;
+        if (!physics) return;
+        physicsWorld.setPos(physics.bodyId, collisionData[1]);
     });
 }
 
@@ -39,22 +44,17 @@ CommandCollisions.prototype.serialize = function(byteArray, index) {
     serializeInt32(byteArray, index, this.collisions.length);
     this.collisions.forEach((function(collisionData) {
         serializeInt32(byteArray, index, collisionData[0]);
-        serializeInt32(byteArray, index, collisionData[1]);
-        serializeV2(byteArray, index, collisionData[2]);
-        serializeV2(byteArray, index, collisionData[3]);
+        serializeV2(byteArray, index, collisionData[1]);
     }).bind(this));
 }
 
 CommandCollisions.prototype.deserialize = function(byteArray, index) {
     var numCollisions = deserializeInt32(byteArray, index);
     for(var i = 0; i < numCollisions; i++) {
-        this.collisions.push([deserializeInt32(byteArray, index), 
-                              deserializeInt32(byteArray, index),
-                              deserializeV2(byteArray, index),
-                              deserializeV2(byteArray, index)]);
+        this.collisions.push([deserializeInt32(byteArray, index), deserializeV2(byteArray, index)]);
     }
 }
 
 CommandCollisions.prototype.getSerializationSize = function() {
-    return 4 + this.collisions.length * (4 + 4 + 8 + 8);
+    return 4 + this.collisions.length * (4 + 8);
 }
