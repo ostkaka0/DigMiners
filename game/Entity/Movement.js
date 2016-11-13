5fd504d64bf6d3163e022a3e2002f041c0f618cc
John <john.3manuelsson@gmail.com>
2016-11-13 17:21:53 +0100

    Movement call item functions

--- game/Entity/Movement.js
+++ game/Entity/Movement.js
@@ -104,7 +104,16 @@ onEntityUseTool = function(gameData, entity) {
         var moveDir = [Math.cos(-angle), Math.sin(-angle)];
         var toolUsePos = [entity.physicsBody.pos[0] + 1.0 * moveDir[0], entity.physicsBody.pos[1] + 1.0 * moveDir[1]];
 
-        // Check if any player/monster at dig position, hit it
+        if (!entity.controlledByPlayer) return;
+        var playerId = entity.controlledByPlayer.playerId;
+        var player = gameData.playerWorld.objects[playerId];
+        if (!player) return;
+        var tool = player.inventory.getEquippedItemType("tool");
+        if (!tool || !tool.itemFunction) return;
+        tool.itemFunction(entity, tool);
+        
+
+        /*// Check if any player/monster at dig position, hit it
         var shortestDistance = Number.MAX_VALUE;
         var shortestDistanceEntity = null;
         gameData.entityWorld.objectArray.forEach(function(otherEntity) {
@@ -155,6 +164,6 @@ onEntityUseTool = function(gameData, entity) {
             //TODO: store monster dig radius constant somewhere
             var command = new CommandDig(entity.physicsBody.pos[0], entity.physicsBody.pos[1], 5.0);
             sendCommand(command);
-        }
+        }*/
     }
 }
