3bfcffbb3d08d22dcba5edb5987d7e35744a40ef
John <john.3manuelsson@gmail.com>
2016-11-13 19:04:35 +0100

    Pathfinding fixes

---
+++ engine/DisFieldFunctions.js
@@ -0,0 +1,72 @@
+DisField = {};
+
+DisField.getPageAndIndex = function(disField, pos) {
+    var pageX = Math.floor(pos[0] / PATH_PAGE_DIM);
+    var pageY = Math.floor(pos[1] / PATH_PAGE_DIM);
+    var localX = Math.floor(pos[0]) - pageX * PATH_PAGE_DIM;
+    var localY = Math.floor(pos[1]) - pageY * PATH_PAGE_DIM;
+    var page = disField.get(pageX, pageY);
+    if (!page) {
+        page = new Uint16Array(PATH_PAGE_DIM * PATH_PAGE_DIM);
+        page.fill(65535);
+        disField.set(pageX, pageY, page);
+    }
+    return [page, localX + localY * PATH_PAGE_DIM];
+}
+
+DisField.getTileDis = function(disField, tilePos) {
+    var pageAndIndex = DisField.getPageAndIndex(disField, tilePos)
+    var page = pageAndIndex[0];
+    var index = pageAndIndex[1];
+    return Math.min(1024, page[index]);
+}
+
+DisField.calcDis = function(disField, pos) {
+    var x1 = Math.floor(pos[0]-0.5);
+	var y1 = Math.floor(pos[1]-0.5);
+	var x2 = x1 + 1;
+	var y2 = y1 + 1;
+		
+	var fractX = pos[0] - x1;
+	var fractY = pos[1] - y1;
+	
+	var a = [
+		1.0 - fractX,
+		1.0 - fractY,
+		fractX,
+		fractY
+	];
+	var b = [
+		fix.div(DisField.getTileDis(disField, [x1, y1]), 10),
+		fix.div(DisField.getTileDis(disField, [x2, y1]), 10),
+		fix.div(DisField.getTileDis(disField, [x1, y2]), 10),
+		fix.div(DisField.getTileDis(disField, [x2, y2]), 10)
+	];
+	
+	return a[0] * a[1] * b [0] +
+		   a[2] * a[1] * b [1] +
+		   a[0] * a[3] * b [2] +
+		   a[2] * a[3] * b [3];
+}
+
+DisField.calcDir = function(disField, pos) {
+    var epsilon = toFix(0.5);
+	var a = -DisField.calcDis(disField, v2.create(pos[0]+epsilon, pos[1]));
+	var b = -DisField.calcDis(disField, v2.create(pos[0]-epsilon, pos[1]));
+	var c = -DisField.calcDis(disField, v2.create(pos[0], pos[1]+epsilon));
+	var d = -DisField.calcDis(disField, v2.create(pos[0], pos[1]-epsilon));
+	
+	var f = [Math.max(-1, Math.min(1, +a-b)), 0];
+	var g = [0, 0];
+	var h = [0, Math.max(-1, Math.min(1, +c-d))];
+	var i = [0, 0];
+	
+	var vec = v2.create(0, 0);
+	v2.add(vec, f, vec);
+	v2.add(vec, g, vec);
+	v2.add(vec, h, vec);
+	v2.add(vec, i, vec);
+	v2.normalize(vec, vec);
+	
+	return vec;
+}
--- game/Entity/AI/TargetPlayerBehaviour.js
+++ game/Entity/AI/TargetPlayerBehaviour.js
@@ -28,37 +28,15 @@ TargetPlayerBehaviour.prototype.run = function() {
     this.flowField = new Map2D();
     aStarFlowField(this.flowField, [], gameData.tileWorld, gameData.blockWorld, tilePos, tilePosTarget);
     //}
+    
+    var dir = DisField.calcDir(this.flowField, [this.entity.physicsBody.pos[0], this.entity.physicsBody.pos[1]]);
 
-    var getPageAndIndex = (function(pos) {
-        var pageX = Math.floor(pos[0] / PATH_PAGE_DIM);
-        var pageY = Math.floor(pos[1] / PATH_PAGE_DIM);
-        var localX = Math.floor(pos[0]) - pageX * PATH_PAGE_DIM;
-        var localY = Math.floor(pos[1]) - pageY * PATH_PAGE_DIM;
-        var page = this.flowField.get(pageX, pageY);
-        if (!page) {
-            page = new Uint16Array(PATH_PAGE_DIM * PATH_PAGE_DIM);
-            page.fill(65535);
-            this.flowField.set(pageX, pageY, page);
-        }
-        return [page, localX + localY * PATH_PAGE_DIM];
-    }).bind(this);
+    //var diffX = dir[0];//(getDis([tilePos[0] - 1, tilePos[1]]) - getDis([tilePos[0] + 1, tilePos[1]])) / 10;
+    //var diffY = dir[1];//(getDis([tilePos[0], tilePos[1] - 1]) - getDis([tilePos[0], tilePos[1] + 1])) / 10;
 
-    var getDis = (function(pos) {
-        var pageAndIndex = getPageAndIndex(pos);
-        var page = pageAndIndex[0];
-        var index = pageAndIndex[1];
-        if (page)
-            return page[index];
-        else
-            return 65535;
-    }).bind(this);
-
-    var diffX = (getDis([tilePos[0] - 1, tilePos[1]]) - getDis([tilePos[0] + 1, tilePos[1]])) / 10;
-    var diffY = (getDis([tilePos[0], tilePos[1] - 1]) - getDis([tilePos[0], tilePos[1] + 1])) / 10;
-
-    var dir = v2.create(diffX, diffY);
-    var normalized = v2.create(0, 0);
-    v2.normalize(dir, normalized);
+    //var dir = v2.create(diffX, diffY);
+    //var normalized = v2.create(0, 0);
+    //v2.normalize(dir, normalized);
 
     var dist = v2.distance(this.entity.physicsBody.pos, this.target.physicsBody.pos);
     /*if(dist < 1.5) // 1.0 limit for punch
@@ -66,7 +44,7 @@ TargetPlayerBehaviour.prototype.run = function() {
     else
         dirs.push(MoveDir.DISABLE_SPACEBAR);*/
 
-    sendCommand(new CommandEntityMove(this.entity.id, normalized, this.entity.physicsBody.pos[0], this.entity.physicsBody.pos[1]));
+    sendCommand(new CommandEntityMove(this.entity.id, dir, this.entity.physicsBody.pos[0], this.entity.physicsBody.pos[1]));
     if (dist > this.maxRadius)
         return false;
     return true;
