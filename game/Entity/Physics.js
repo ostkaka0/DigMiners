009d0bf69125b51db00bdbf07cf1530701b0c138
John <john.3manuelsson@gmail.com>
2016-11-13 22:07:54 +0100

    Monsters can dig

--- game/Entity/Physics.js
+++ game/Entity/Physics.js
@@ -49,6 +49,7 @@ Object.defineProperties(PhysicsBody.prototype, {
 });
 
 PhysicsBody.prototype.getPos = function() { return gameData.physicsWorld.getPos(this.bodyId); }
+PhysicsBody.prototype.getPosOld = function() { return gameData.physicsWorld.getPosOld(this.bodyId); }
 PhysicsBody.prototype.setPos = function(pos) { gameData.physicsWorld.setPos(this.bodyId, pos); }
 PhysicsBody.prototype.getVelocity = function() { return gameData.physicsWorld.getVelocity(this.bodyId); }
 PhysicsBody.prototype.setVelocity = function(velocity) { gameData.physicsWorld.setVelocity(this.bodyId, velocity); }
