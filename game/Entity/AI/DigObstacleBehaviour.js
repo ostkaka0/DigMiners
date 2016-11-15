5a3444f7c6c977f6ebf5335dd8e240ebb24c9b52
Ostkaka <ostkaka@localhost.localdomain>
2016-11-15 14:43:56 +0100

    DigObstacleBehaviour improved

--- game/Entity/AI/DigObstacleBehaviour.js
+++ game/Entity/AI/DigObstacleBehaviour.js
@@ -2,18 +2,24 @@
 DigObstacleBehaviour = function(entity) {
     this.entity = entity;
     this.targetTilePos = null;
+    this.oldMoveDir = null;
     this.stopTick = null;
+    this.nextRunTick = null; 
 }
 
 DigObstacleBehaviour.prototype.canRun = function() {
+    if (this.nextRunTick && gameData.tickId < this.nextRunTick)
+        return false;
+    
     var velocity = this.entity.physicsBody.getVelocity();
     var moveDir = this.entity.movement.direction;
-    if (v2.dot(moveDir, velocity) > 0.8)
+    if (v2.dot(moveDir, velocity) > 0.4)
         return false;
     var pos = this.entity.physicsBody.getPos();
-    var tilePos = [Math.floor(pos[0] + moveDir[0]), Math.floor(pos[1] + moveDir[1])];
-    for (var i = 0; i < 9; i++) {
-        var itPos = [tilePos[0] + i % 3 - 1, tilePos[1] + (i / 3 >> 0) - 1];
+    var digPos = [pos[0] + moveDir[0]/2, pos[1] + moveDir[1]/2];
+    var tilePos = [Math.floor(digPos[0] - 0.5), Math.floor(digPos[1] - 0.5)];
+    for (var i = 0; i < 4; i++) {
+        var itPos = [tilePos[0] + (i & 1), tilePos[1] + (i >> 1)];
         var blockId = getForeground(gameData.blockWorld, itPos[0], itPos[1]);
         var density = getDensity(gameData.tileWorld, itPos[0], itPos[1]);
         if (blockId != 0 || density > 127) {
@@ -25,6 +31,7 @@ DigObstacleBehaviour.prototype.canRun = function() {
 }
 
 DigObstacleBehaviour.prototype.initialize = function() {
+    this.oldMoveDir = this.entity.movement.direction;
     sendCommand(new CommandKeyStatusUpdate(this.entity.id, Keys.SPACEBAR, true, this.entity.physicsBody.pos));
     sendCommand(new CommandEntityMove(this.entity.id, [0, 0], this.entity.physicsBody.pos[0], this.entity.physicsBody.pos[1]));
     var pos = this.entity.physicsBody.getPos();
@@ -33,13 +40,16 @@ DigObstacleBehaviour.prototype.initialize = function() {
     var normalized = v2.create(0, 0);
     v2.normalize(diff, normalized);
     sendCommand(new CommandEntityRotate(this.entity.id, normalized));
-    this.stopTick = gameData.tickId + (2000 / gameData.tickDuration >> 0);
+    this.stopTick = gameData.tickId + (4000 / gameData.tickDuration >> 0);
+    this.nextRunTick = null;
 }
 
 DigObstacleBehaviour.prototype.run = function() {
     var blockId = getForeground(gameData.blockWorld, this.targetTilePos[0], this.targetTilePos[1]);
     var density = getDensity(gameData.tileWorld, this.targetTilePos[0], this.targetTilePos[1]);
     if (gameData.tickId >= this.stopTick) {
+        // No digging for 2 seconds
+        this.nextRunTick = gameData.tickId + (2000 / gameData.tickDuration >> 0);
         return false;
     }
 
@@ -60,5 +70,6 @@ DigObstacleBehaviour.prototype.run = function() {
 
 DigObstacleBehaviour.prototype.finish = function() {
     sendCommand(new CommandKeyStatusUpdate(this.entity.id, Keys.SPACEBAR, false, this.entity.physicsBody.pos));
-    sendCommand(new CommandEntityMove(this.entity.id, [0, 0], this.entity.physicsBody.pos[0], this.entity.physicsBody.pos[1]));
+        sendCommand(new CommandEntityMove(this.entity.id, [0, 0], this.entity.physicsBody.pos[0], this.entity.physicsBody.pos[1]));
+    sendCommand(new CommandEntityRotate(this.entity.id, this.oldMoveDir));
 }
