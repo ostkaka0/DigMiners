bb73cd9e12d82c602a7f29656a04fc59ac39d39c
Gustav Hammar <gustav9797@hotmail.se>
2016-11-20 16:51:19 +0100

    Add stayTime to projectiles

--- game/Projectiles.js
+++ game/Projectiles.js
@@ -6,5 +6,6 @@ Projectiles.Egg = {
     speed: 1.25, // blocks per second
     damage: 10.0, // damage per hit
     blockDamage: 5.0,
+    stayTime: 500, // milliseconds projectile will stay after hit
     textureName: "egg"
 }
