b52151fd576e3531f234843f96190403e468709e
Ostkaka <ostkaka@localhost.localdomain>
2016-11-22 18:02:29 +0100

    Better projectiles

--- game/Projectiles.js
+++ game/Projectiles.js
@@ -3,22 +3,22 @@ Projectiles = {};
 
 Projectiles.BigEgg = {
     radius: 1.0,
-    maxDistance: 10, // blocks
-    speed: 0.25, // blocks per second
-    damage: 10.0, // damage per hit
+    maxDistance: 50, // blocks
+    speed: 1.0, // blocks per second
+    damage: 60.0, // damage per hit
     blockDamage: 5.0,
     stayTime: 0, // milliseconds projectile will stay after hit
     textureName: "bigEgg",
     isExplosive: true,
     explosiveRadius: 5.0,
-    explosiveEntityDamage: 200, // damage in HP units at center of explosion
+    explosiveEntityDamage: 60, // damage in HP units at center of explosion
     explosionBlockDamage: 400, // damage in block HP units at center of explosion
     explosionTileDamage: 2 // number of times to run CommandDig with above radius
 }
 
 Projectiles.Egg = {
     radius: 1.0,
-    maxDistance: 10, // blocks
+    maxDistance: 200, // blocks
     speed: 1.75, // blocks per second
     damage: 10.0, // damage per hit
     blockDamage: 0.0,
