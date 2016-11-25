fa6f3cfb12b13c7c7fc0c2131860ba87e56be873
Gustav Hammar <gustav9797@hotmail.se>
2016-11-25 15:42:51 +0100

    Created door particle

--- game/Particles.js
+++ game/Particles.js
@@ -84,3 +84,45 @@ Particles.BigEgg = {
         "r": 30
     }
 }
+
+Particles.Door = {
+    "alpha": {
+        "start": 0.1,
+        "end": 0.1
+    },
+    "scale": {
+        "start": 1,
+        "end": 0.4
+    },
+    "color": {
+        "start": "#000000",
+        "end": "#000000"
+    },
+    "speed": {
+        "start": 10,
+        "end": 10
+    },
+    "startRotation": {
+        "min": 0,
+        "max": 360
+    },
+    "rotationSpeed": {
+        "min": 0,
+        "max": 0
+    },
+    "lifetime": {
+        "min": 0.1,
+        "max": 0.3
+    },
+    "blendMode": "normal",
+    "frequency": 0.001,
+    "emitterLifetime": 0,
+    "maxParticles": 1000,
+    "addAtBack": true,
+    "spawnType": "circle",
+    "spawnCircle": {
+        "x": 0,
+        "y": 0,
+        "r": 14
+    }
+}
