301023b3deec927cc4956729f7b9c6393603f7b8
Gustav Hammar <gustav9797@hotmail.se>
2017-01-11 18:12:29 +0100

    Class picking buttons are automatically generated.

--- game/PlayerClass.js
+++ game/PlayerClass.js
@@ -3,24 +3,24 @@ initPlayerClasses = function() {
         Assault: {
             name: "Assault",
             blocks: [Items.StoneWall, Items.WoodCrate, Items.BlueForcefield],
-            weapons: [Items.WeaponMachineGun, Items.WeaponPistol],
+            weapons: [Items.WeaponAssaultRifle, Items.WeaponPistol],
             speed: 1.0,
             health: 120.0,
         },
         Heavy: {
             name: "Heavy",
             blocks: [Items.StoneWall, Items.WoodCrate, Items.BlueForcefield],
-            weapons: [Items.WeaponAssaultRifle, Items.WeaponPistol],
+            weapons: [Items.WeaponMachineGun, Items.WeaponPistol],
             speed: 0.7,
             health: 150.0,
-        }, 
+        },
         Sniper: {
             name: "Sniper",
             blocks: [Items.StoneWall, Items.BunkerWindow, Items.BlueForcefield],
             weapons: [Items.WeaponSniperRifle, Items.WeaponPistol],
             speed: 1.2,
             health: 100.0,
-        }, 
+        },
         Medic: {
             name: "Medic",
             blocks: [Items.StoneWall, Items.BunkerWindow, Items.HealthBox],
