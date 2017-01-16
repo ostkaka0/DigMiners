59255d34a1c6c2c5ec1d9ef1ca2a687ffcb6fd81
Gustav Hammar <gustav9797@hotmail.se>
2017-01-16 13:03:00 +0100

    Better name generator.

--- game/Message/ToServer/MessageRequestSpawn.js
+++ game/Message/ToServer/MessageRequestSpawn.js
@@ -8,10 +8,13 @@ MessageRequestSpawn.prototype.execute = function(gameData, player) {
     if (player.entity != null && player.entityId != null) return;
     if (gameData.world.tickId - player.deathTick < 20 * Config.respawnTime) return;
 
+    if (this.playerName && this.playerName.length > 0)
+        player.name = this.playerName;
+
     var entityId = gameData.world.idList.next();
     var classType = PlayerClassRegister[this.classId];
     var teamId = gameData.gameMode.teams[Math.random() * gameData.gameMode.teams.length >> 0];
-    var entity = entityTemplates.player(player.id, entityId, this.playerName, classType, teamId);
+    var entity = entityTemplates.player(player.id, entityId, player.name, classType, teamId);
 
     // Set spawn position
     var pos = gameData.gameMode.playerSpawns[teamId][Math.random() * gameData.gameMode.playerSpawns[teamId].length >> 0];
@@ -20,7 +23,7 @@ MessageRequestSpawn.prototype.execute = function(gameData, player) {
 
     console.log("spawning entity " + entityId + " at " + pos);
     sendCommand(new CommandEntitySpawn(gameData, entity, entityId));
-    sendCommand(new CommandPlayerSpawn(player.id, entityId, this.playerName));
+    sendCommand(new CommandPlayerSpawn(player.id, entityId, player.name));
 
     classType.weapons.forEach(function(weapon) {
         sendCommand(new CommandEntityInventory(entityId, InventoryActions.ADD_ITEM, weapon.id, 1));
