a447a78c3938bcd150850cdb474d551da0faf63d
Gustav Hammar <gustav9797@hotmail.se>
2017-01-18 20:25:58 +0100

    Added adsense code

--- html_index.php
+++ html_index.php
@@ -1,5 +1,12 @@
 <html>
     <head>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-9476867787742374",
+        enable_page_level_ads: true
+    });
+    </script>
     <title>DigMiners</title>
     <link rel="stylesheet" type="text/css" href="tether.min.css"/>
     <link rel="stylesheet" type="text/css" href="bootstrap.min.css"/>
