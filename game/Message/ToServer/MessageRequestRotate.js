e649301f5ff89f4ba83f4c02b2acc3b941ee0664
Gustav Hammar <gustav9797@hotmail.se>
2017-01-22 19:05:45 +0100

    Calc rotation automatically

--- game/Message/ToServer/MessageRequestRotate.js
+++ game/Message/ToServer/MessageRequestRotate.js
@@ -12,18 +12,26 @@ MessageRequestRotate.prototype.execute = function(gameData, player) {
     var movement = entity.movement;
     if (!movement) return;
     if (v2.length(this.deltaWorldCursorPos) < 0.1) return;
-    
+
     // Calculate direction:
     var pos = entity.physicsBody.getPos();
     var direction = [0, 0];
     v2.normalize(this.deltaWorldCursorPos, direction);
-    var diffLen = 2*v2.length(this.deltaWorldCursorPos);
+    var diffLen = 2 * v2.length(this.deltaWorldCursorPos);
     if (diffLen < 0.1) return;
-    var armLength = 0.5; // The distance from origin to weapon
-    var rotateAngle = Math.acos(Math.sqrt(diffLen*diffLen-armLength*armLength)/diffLen);
+
+    entity.drawable.positionAll(0, 0, 0, entity.bodyparts);
+    var tool = entity.bodyparts.bodyparts["tool"];
+    var toolUsePos = [0, 0];
+    v2.add(toolUsePos, tool.finalPos, toolUsePos);
+    toolUsePos = [toolUsePos[0], -toolUsePos[1]];
+    v2.mul(1 / 32, toolUsePos, toolUsePos);
+    var armLength = -toolUsePos[1] * 2; // The distance from origin to weapon
+
+    var rotateAngle = Math.acos(Math.sqrt(diffLen * diffLen - armLength * armLength) / diffLen);
     direction = [Math.cos(rotateAngle) * direction[0] - Math.sin(rotateAngle) * direction[1], Math.cos(rotateAngle) * direction[1] + Math.sin(rotateAngle) * direction[0]];
     if (isNaN(direction[0]) || isNaN(direction[1])) return;
-    
+
     movement.rotationDirection = direction;
     movement.deltaWorldCursorPos = this.deltaWorldCursorPos;
     sendCommand(new CommandEntityRotate(entity.id, direction));
