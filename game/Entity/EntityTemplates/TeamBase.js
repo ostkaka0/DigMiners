65930cef86c4047ac9deb9c9a250d8c9e999cd99
Gustav Hammar <gustav9797@hotmail.se>
2017-01-27 10:35:19 +0100

    Split EntityTemplates.js into separate files.

---
+++ game/Entity/EntityTemplates/TeamBase.js
@@ -0,0 +1,37 @@
+
+entityTemplates.TeamBase = function(entityId, pos, teamId, maxEntities, radius, duration) {
+    maxEntities = maxEntities || 10;
+    radius = radius || 4.0;
+    duration = duration || 60;
+
+    var entity = {};
+    entity.spawner = new Spawner(entityTemplates.Monster, pos, maxEntities, radius, duration, [{ id: Items.WeaponPistol.id, quantity: 1 }, { id: Items.RustyShovel.id, quantity: 1 }, { id: Items.Egg.id, quantity: 1000 }], null, null, teamId);
+    entity.physicsBody = new PhysicsBody(v2.create(pos[0], pos[1]), 0.01, null, 1000.0);
+    entity.nameComponent = new NameComponent("Base");
+
+    var teamEnum = teamId;
+    var feetSprite = new Sprite("monster/feet.png");
+    var rightArmSprite = new Sprite("monster/rightArm.png");
+    var leftArmSprite = new Sprite("monster/leftArm.png");
+    var headSprite = new Sprite("monster/head.png");
+    var hatSprite = new Sprite((teamEnum == Teams.Blue) ? "egg.png" : "bigEgg.png");
+
+    var bodyparts = {
+        "feet": new BodyPart(feetSprite, 0, 0, 0, null, null),
+        "tool": new BodyPart(new Sprite(), -10, 15, 0, null, "rightArm"),
+        "rightArm": new BodyPart(rightArmSprite, 5, 8, 0, [10, 11], "body"),
+        "leftArm": new BodyPart(leftArmSprite, 5, -8, 0, [10, 23], "body"),
+        "body": new BodyPart(new Sprite(), 0, 0, 0, null, null),
+        "head": new BodyPart(headSprite, 1, 0, 0, null, null),
+        "hat": new BodyPart(hatSprite, 1, 0, 0, null, null)
+    };
+
+    entity.bodyparts = new Bodyparts(bodyparts);
+    entity.drawable = new Drawable(1);
+    var healthbarSprite = new Sprite("healthbar.png", null, false);
+    entity.drawable.addSprite("healthbar", healthbarSprite, v2.create(0, -35), false, true);
+
+    entity.health = new Health(2000, 2000, 0.0);
+    entity.team = new Team(teamEnum);
+    return entity;
+}
