88b858280c2a2caf750202f87f5f0965462da36a
Gustav Hammar <gustav9797@hotmail.se>
2017-01-31 18:03:41 +0100

    Fixed Generator.js, made perlin.js module

--- Game/Generator.js
+++ Game/Generator.js
@@ -6,6 +6,8 @@ var Global = require("Game/Global.js")
 var Blocks = require("Game/Blocks.js")
 var Tiles = require("Game/Blocks.js")
 
+noise = require("lib/perlin.js").noise;
+
 var Generator = function(seed) {
     if (!seed)
         seed = 0;
@@ -26,15 +28,15 @@ Generator.prototype.generate = function(chunk, chunkX, chunkY) {
             var distance = Math.sqrt(x * x + y * y) / 200.0
             distance -= 0.25;
 
-            //noise.seed(this.seed);
-            var value = Math.random();//noise.perlin2(x / 20.0, y / 20.0);
+            noise.seed(this.seed);
+            var value = noise.perlin2(x / 20.0, y / 20.0);
             value += distance;
-            //noise.seed(this.oreNoise1);
-            var oreValue1 = Math.random();//noise.perlin2(x / 4.0, y / 4.0);
-            //noise.seed(this.oreNoise2);
-            var oreValue2 = Math.random();//noise.perlin2(x / 4.0, y / 4.0);
-            //noise.seed(this.oreNoise3);
-            var oreValue3 = Math.random();//noise.perlin2(x / 4.0, y / 4.0);
+            noise.seed(this.oreNoise1);
+            var oreValue1 = noise.perlin2(x / 4.0, y / 4.0);
+            noise.seed(this.oreNoise2);
+            var oreValue2 = noise.perlin2(x / 4.0, y / 4.0);
+            noise.seed(this.oreNoise3);
+            var oreValue3 = noise.perlin2(x / 4.0, y / 4.0);
 
             var tileId = 0;
 
