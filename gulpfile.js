f6a547b73e7632798f23b021567017a856327bc0
John <john.3manuelsson@gmail.com>
2017-01-31 16:49:15 +0100

    npm run watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require("gulp");
 var rollup = require("rollup-stream");
 var source = require("vinyl-source-stream");
 var nodeResolve = require("rollup-plugin-node-resolve");
+var browserify = require("browserify");
+var fs = require("fs");
+var path = require("path");
+var copydir = require('copy-dir');
 
 gulp.task("rollup", function() {
   return rollup({
-      entry: "DigMiners.js"),//"./tests/TestPointWorld.js"),
+      entry: "DigMiners.js",//"./tests/TestPointWorld.js"),
       plugins: [
         nodeResolve({
           /*module: true,
@@ -15,15 +19,44 @@ gulp.task("rollup", function() {
           browser: true,  // Default: false
           extensions: [ ".js")],
           preferBuiltins: false  // Default: true*/
-        })
+      })
       ]
     })
-    .pipe(source("src.js")))
+    .pipe(source("src.js"))
     .pipe(gulp.dest("./html"));
+});
+
+gulp.task("browserify", function() {
+    var b = browserify();
+    b.add("DigMiners.js");
+    b.bundle()
+        .pipe(source("src.js"))
+        .pipe(gulp.dest("./html"));
+});
+
+gulp.task("copy", function() {
+    var copyFile = function (src, dest) {
+        dir = path.dirname(dest);
+        if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+        fs.createReadStream(src).pipe(fs.createWriteStream(dest));
+    }
+    var copyRecursive = function(dir, dest) {
+        console.log(dir, "copying to", dest);
+        copydir(dir, dest, function(error) {
+            if (error) console.log("Copy error: " + error);
+        });
+    }
+    var outputPath = "html/";
+
+    copyFile("html_index.php", outputPath + "index.php");
+    copyFile("style.css", outputPath + "style.css");
+    copyFile("bootstrap.min.css", outputPath + "bootstrap.min.css");
+    copyFile("tether.min.css", outputPath + "tether.min.css");
+    copyRecursive("data/", outputPath + "data");
 });
 
 
 gulp.task("watch", function() {
-    gulp.watch(["Engine/**/*.js"), "Game/**/*.js"), "tests/**/*.js"), "DigMiners.js")], ["rollup"]);
+    gulp.watch(["Engine/**/*.js", "Game/**/*.js", "tests/**/*.js", "DigMiners.js"], ["browserify"]);
 });
-gulp.task("default", [ "rollup", "watch" ]);
+gulp.task("default", [ "browserify", "watch", "copy" ]);
