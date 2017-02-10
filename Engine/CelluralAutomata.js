b747537ccc21a1dce6f9de74c78b2ee40e08e36f
John <john.3manuelsson@gmail.com>
2017-02-10 12:23:50 +0100

    CelluralAutomata.js(unfinished)

---
+++ Engine/CelluralAutomata.js
@@ -0,0 +1,40 @@
+export default class {
+    constructor(blockWorld, blockRegister) {
+        this.blockWorld = blockWorld;
+        this.blockRegister = blockRegister;
+        this.updatingBlocks = {};
+        this.blocks = new Map2D();
+
+        this.initEvents();
+    }
+
+    tick() {
+        var updatingBlocks = this.updatingBlocks;
+        this.updatingBlocks = [];
+        this.blocks = {};
+
+        updatingBlocks.forEach((blockId) => {
+            var blockList = updatingBlocks[blockId];
+            var blockType = this.blockRegister[blockId];
+            if (blockType.updateCells)
+                blockType.updateCells(this, this.blockWorld, blockList);
+        });
+    }
+
+    updateBlock(blockPos, blockId) {
+        if (this.blocks.get(blockPos.x, blockPos.y)) return;
+        var blockType = this.blockRegister[blockId];
+        if (!blockType || !blockType.updateCells) return;
+        this.blocks.set(blockPos[0], blockPos[1], blockId);
+        if (!this.updatingBlocks[blockId])
+            this.updatingBlocks[blockId] = [blockPos];
+        else
+            this.updatingBlocks[blockId].push(blockPos);
+    }
+
+    initEvents() {
+        Event.subscribe(this.blockWorld.events.onPlace, this, (blockPos, blockId) => this.updateBlock(blockPos, blockId));
+        Event.subscribe(this.blockWorld.events.onHardnessChange, this, (blockPos, blockId) => this.updateBlock(blockPos, blockId));
+    }
+
+}
