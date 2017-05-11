9d786df7667f2ef08a408a285037dd4903a9be10
Gustav Hammar <gustav9797@hotmail.se>
2017-05-11 14:27:37 +0200

    Nicer generator

--- Game/Generator/SurvivalGenerator.js
+++ Game/Generator/SurvivalGenerator.js
@@ -7,14 +7,17 @@ var SurvivalGenerator = function(seed) {
 }
 
 SurvivalGenerator.prototype.generate = function(chunk, chunkX, chunkY) {
-    var grassBorder = Noise.betterRand(this.seed) * 20 + 15;
+    //var grassBorder = Noise.betterRand(this.seed) * 20 + 15;
     for (var yy = 0; yy < Chunk.dim; ++yy) {
         for (var xx = 0; xx < Chunk.dim; ++xx) {
             var x = xx + chunkX * Chunk.dim;
             var y = yy + chunkY * Chunk.dim;
 
             var distance = Math.sqrt(x * x + y * y);
-            if (distance < grassBorder) {
+            noise.seed(this.seed);
+            var value = noise.perlin2(x / 20.0, y / 20.0);
+            value += distance / 20.0;
+            if (value < 2.0) {
                 chunk.setTileId(xx, yy, Tiles.Grass.id);
                 chunk.setDensity(xx, yy, 0);
                 if (isServer) {
@@ -24,8 +27,17 @@ SurvivalGenerator.prototype.generate = function(chunk, chunkX, chunkY) {
                         sendCommand(new CommandEntitySpawn(gameData, tree, treeEntityId));
                     }
                 }
-            } else
+            } else {
                 chunk.setTileId(xx, yy, Tiles.Dirt.id);
+
+                noise.seed(this.seed);
+                var value = noise.perlin2(x / 20.0, y / 20.0);
+                value += distance / 200.0;
+
+                if (value > 0.3) {
+                    chunk.setTileId(xx, yy, 1);
+                }
+            }
         }
     }
 }
