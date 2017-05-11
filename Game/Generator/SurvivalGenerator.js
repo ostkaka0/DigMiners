621b60c89464a1cbd524f707582068fd331ca1a2
Gustav Hammar <gustav9797@hotmail.se>
2017-05-11 14:44:25 +0200

    Survival terrain gen done

--- Game/Generator/SurvivalGenerator.js
+++ Game/Generator/SurvivalGenerator.js
@@ -7,7 +7,6 @@ var SurvivalGenerator = function(seed) {
 }
 
 SurvivalGenerator.prototype.generate = function(chunk, chunkX, chunkY) {
-    //var grassBorder = Noise.betterRand(this.seed) * 20 + 15;
     for (var yy = 0; yy < Chunk.dim; ++yy) {
         for (var xx = 0; xx < Chunk.dim; ++xx) {
             var x = xx + chunkX * Chunk.dim;
@@ -16,8 +15,8 @@ SurvivalGenerator.prototype.generate = function(chunk, chunkX, chunkY) {
             var distance = Math.sqrt(x * x + y * y);
             noise.seed(this.seed);
             var value = noise.perlin2(x / 20.0, y / 20.0);
-            value += distance / 20.0;
-            if (value < 2.0) {
+            value += distance / 40.0;
+            if (value < 1.0) {
                 chunk.setTileId(xx, yy, Tiles.Grass.id);
                 chunk.setDensity(xx, yy, 0);
                 if (isServer) {
@@ -29,6 +28,8 @@ SurvivalGenerator.prototype.generate = function(chunk, chunkX, chunkY) {
                 }
             } else {
                 chunk.setTileId(xx, yy, Tiles.Dirt.id);
+                if (value < 1.5)
+                    continue;
 
                 noise.seed(this.seed);
                 var value = noise.perlin2(x / 20.0, y / 20.0);
@@ -36,6 +37,20 @@ SurvivalGenerator.prototype.generate = function(chunk, chunkX, chunkY) {
 
                 if (value > 0.3) {
                     chunk.setTileId(xx, yy, 1);
+
+                    noise.seed(this.seed + 1);
+                    var oreValue1 = noise.perlin2(x / 8.0, y / 8.0);
+                    noise.seed(this.seed + 331);
+                    var oreValue2 = noise.perlin2(x / 4.0, y / 4.0);
+                    noise.seed(this.seed + 71117);
+                    var oreValue3 = noise.perlin2(x / 4.0, y / 4.0);
+
+                    if (oreValue1 > 0.40)
+                        chunk.setTileId(xx, yy, Tiles.Coal.id);
+                    if (oreValue2 > 0.50)
+                        chunk.setTileId(xx, yy, Tiles.Copper.id);
+                    if (oreValue3 > 0.6)
+                        chunk.setTileId(xx, yy, Tiles.Iron.id);
                 }
             }
         }
